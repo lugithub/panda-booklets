1fd0937d86f54bb22da981333729adc03290e479
Bin Lu <lu.bin.lu.bin@gmail.com>
2019-04-10 11:00:50 -0400

    wisc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import random from 'random';
 import logo from './IMG_20190216_154755327.jpg';
 import './App.css';
 
@@ -10,6 +11,41 @@ function Question({ task }) {
   );
 }
 
+function generateNumbers(count) {
+  let numbers = [];
+  while (numbers.length < count) {
+    const x = random.uniformInt(1, 9)();
+    if (numbers.find(j => j === x)) {
+      continue;
+    }
+    numbers.push(x);
+  }
+  return numbers;
+}
+
+function RepeatNumbers() {
+  const dummmy = [0, 0, 0, 0];
+  return (
+    <ul>
+      {dummmy.map(() => (
+        <li>{generateNumbers(4).join(', ')}</li>
+      ))}
+      {dummmy.map(() => (
+        <li>{generateNumbers(5).join(', ')}</li>
+      ))}
+      {dummmy.map(() => (
+        <li>{generateNumbers(6).join(', ')}</li>
+      ))}
+      {dummmy.map(() => (
+        <li>{generateNumbers(7).join(', ')}</li>
+      ))}
+      {dummmy.map(() => (
+        <li>{generateNumbers(8).join(', ')}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App({ tasks, on }) {
   return (
     <div className="App">
@@ -17,6 +53,17 @@ function App({ tasks, on }) {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         Panda practice on {on}
       </header>
+      <article className="wisc">
+        <h5>
+          I will say some numbers for only one time. When i stop, you repeat
+          them to me in the same order just the way i said.
+        </h5>
+        <h5>Say the number backwards</h5>
+        <h5>Say the numbers in order, starting from the smallest</h5>
+        <RepeatNumbers />
+        <RepeatNumbers />
+        <RepeatNumbers />
+      </article>
       <article className="booklet">
         {tasks.map((task, i) => (
           <Question task={task} key={i} />
